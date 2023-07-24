e883c92d6d67def783a1c727e4c7b94a9b0e0e92
Laura McGowan <prime.lmcg@gmail.com>
2023-07-24 16:29:22 -0500

    basic functions outlines written / inputs & options written and working

--- src/App.js
+++ src/App.js
@@ -2,33 +2,56 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div>
       <h1>Tip Calculator</h1>
-
-      <h2>How much is the bill?</h2>
-      <input type="text"></input>
-
-      <h2>How do you rate the service?</h2>
-      <select>
-        <option>It was not good (0%)</option>
-        <option>It was okay (5%)</option>
-        <option>It was good (10%)</option>
-        <option>It was great!!(20%)</option>
-      </select>
-
-      <h2>How does your friend rate the service?</h2>
-      <select>
-        <option>It was not good (0%)</option>
-        <option>It was okay (5%)</option>
-        <option>It was good (10%)</option>
-        <option>It was great!!(20%)</option>
-      </select>
-
-      <div>
-        <button>Reset</button>
-      </div>
-    </>
+      <TipCalculator />
+    </div>
   );
+}
+
+function TipCalculator() {
+  return (
+    <div>
+      <BillInput />
+      <SelectPercentage>How do you rate the service?</SelectPercentage>
+      <SelectPercentage>
+        How does your friend rate the service?
+      </SelectPercentage>
+      <Output />
+      <Reset />
+    </div>
+  );
+}
+
+function BillInput() {
+  return (
+    <div>
+      <label>How much is the bill?</label>
+      <input type="text" placeholder="total bill"></input>
+    </div>
+  );
+}
+
+function SelectPercentage({ children }) {
+  return (
+    <div>
+      <label>{children}</label>
+      <select>
+        <option value="0">It was not good (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">It was great!!(20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Output() {
+  <h3>You pay X (A + B)</h3>;
+}
+
+function Reset() {
+  <button>Reset</button>;
 }
 
 export default App;
